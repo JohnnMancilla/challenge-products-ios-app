9b8ac7d4066cb0d6f3f9dcc89df946b5b84f83d2
johnn <johnn.david.mancilla.zarate@gmail.com>
2026-01-14 18:20:01 -0300

    Initial Commit

---
+++ ChallengeProductsApp/ChallengeProductsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeProductsAppApp.swift
+//  ChallengeProductsApp
+//
+//  Created by Johnn Mancilla on 14-01-26.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeProductsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
